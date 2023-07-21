d2698cbd29ada9e360d62346857309ee129c9bc7
Shepard94 <124931439+Shepard94@users.noreply.github.com>
2023-07-21 11:44:33 -0400

    Add files via upload

---
+++ 4403movieSPAscript.js
@@ -0,0 +1,28 @@
+function search() {
+    var searchInput = document.getElementById('searchInput').value;
+    var url = "https://api.themoviedb.org/3/search/multi?api_key=YOUR_API_KEY&query=" + searchInput;
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        var results = document.getElementById('searchResults');
+        results.innerHTML = '';
+
+        data.results.forEach(result => {
+            var title = result.title || result.name;
+            var poster = result.poster_path ? 'https://image.tmdb.org/t/p/w200' + result.poster_path : 'no_image_available.jpg';
+            var overview = result.overview;
+
+            var html = '<div class="result">' +
+                '<img src="' + poster + '">' +
+                '<h3>' + title + '</h3>' +
+                '<p>' + overview + '</p>' +
+                '</div>';
+
+            results.innerHTML += html;
+        });
+    })
+    .catch(error => {
+        console.log(error);
+    });
+}
